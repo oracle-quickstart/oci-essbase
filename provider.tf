47d143353d25c17b085c6339a30c7d597afcbba5
Anil Arora <anil.arora@oracle.com>
2020-02-19 00:24:16 -0700

    Updated provider.tf file

--- provider.tf
+++ provider.tf
@@ -3,7 +3,9 @@
 
 provider "oci" {
   version = "~> 3.53.0"
-  region  = var.region
-  auth    = "InstancePrincipal"
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+  # user_ocid        = "${var.user_ocid}"
+  # fingerprint      = "${var.fingerprint}"
+  # private_key_path = "${var.private_key_path}"
 }
-

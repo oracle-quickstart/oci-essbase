225120c94903359a9c7d13e029d19aa421ddd7b4
Anil Arora <anilarora@users.noreply.github.com>
2020-02-19 10:50:42 -0700

    Synchronize changes (#12)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,38 @@
 ## Copyright (c) 2020, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
+variable "tenancy_ocid" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "compartment_ocid" {
+  type = string
+}
+
+variable "user_ocid" {
+  type    = string
+  default = ""
+}
+
+variable "fingerprint" {
+  type    = string
+  default = ""
+}
+
+variable "private_key_path" {
+  type    = string
+  default = ""
+}
+
 provider "oci" {
   version = "~> 3.53.0"
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
-  # user_ocid        = "${var.user_ocid}"
-  # fingerprint      = "${var.fingerprint}"
-  # private_key_path = "${var.private_key_path}"
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
 }

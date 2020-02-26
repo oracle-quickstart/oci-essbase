77336fc91ca772c7f06f22993dc5c87e0ba0c05b
Anil Arora <anil.arora@oracle.com>
2020-02-26 13:48:14 -0700

    Update version

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,10 @@
 ## Copyright (c) 2020, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
+terraform {
+  required_version = "< 0.12"
+}
+
 provider "local" {
   version = "~> 1.2"
 }

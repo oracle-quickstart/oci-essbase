3c41f4b9384f85ff4962d43541c9bdb4659d579a
Anil Arora <anilarora@users.noreply.github.com>
2020-03-25 16:18:41 -0600

    Fix terraform validation and workflow (#43)
    Removed duplicate terraform variables and update validation workflow.

--- terraform/oci.tf
+++ terraform/oci.tf
@@ -1,15 +1,6 @@
 ## Copyright (c) 2020, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-variable "tenancy_ocid" {
-}
-
-variable "region" {
-}
-
-variable "compartment_ocid" {
-}
-
 variable "user_ocid" {
   default = ""
 }

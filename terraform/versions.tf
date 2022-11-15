9911b6ebbccd14b69a51afaff3fab9e6d136f7c1
Sankara Narayanan H <sankara.n.narayanan@oracle.com>
2022-11-15 15:02:53 +0530

    template version removed. was creating error while init

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -16,10 +16,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.1"
     }
-    #template = {
-     # source  = "hashicorp/template"
-      #version = "~> 2.1"
-    #}
     tls = {
       source  = "hashicorp/tls"
       version = "~> 3.1"
